f447c4e06f4bb093a9e88ce53ae05191788d4802
julianarhee <juliana.rhee@gmail.com>
2015-05-26 14:37:19 -0400

    reorganized files

--- analysis/fieldmap.py
+++ analysis/fieldmap.py
@@ -9,7 +9,7 @@ import numpy.fft as fft
 import sys
 
 sampling_rate = 60.0
-reduce_factor = (4, 4)
+reduce_factor = (1, 1)
 cache_file = True
 target_freq = 0.1
 binspread = 0
@@ -19,13 +19,19 @@ imdir = sys.argv[1]
 files = os.listdir(imdir)
 files = [f for f in files if os.path.splitext(f)[1] == '.png']
 
+# files = files[0:int(round(len(files)*0.5))]
+
 sample = imread(os.path.join(imdir, files[0]))
+sample = sample[30:-1, 50:310]
+print "FIRST", sample.dtype
 sample = block_reduce(sample, reduce_factor)
 
+# plt.figure()
 # plt.imshow(sample)
 # plt.show()
 
 stack = np.empty((sample.shape[0], sample.shape[1], len(files)))
+print len(files)
 
 print('copying files')
 
@@ -38,8 +44,11 @@ for i, f in enumerate(files):
 	if i % 100 == 0:
 		print('%d images processed...' % i)
 	im = imread(os.path.join(imdir, f)).astype('float')
+	# print im.shape
+	im = im[30:-1, 50:310]
+	# print im.shape
 	im_reduced = block_reduce(im, reduce_factor)
-
+	# im_reduced = im
 	stack[:,:,i] = im_reduced
 
 
@@ -78,13 +87,13 @@ for x in range(sample.shape[0]):
 			phase_map[x, y] = phase[target_bin]
 
 
-plt.subplot(2, 1, 1)
-plt.imshow(mag_map)
-plt.colorbar()
-plt.subplot(2, 1, 2)
+# plt.subplot(2, 1, 1)
+# plt.imshow(mag_map)
+# plt.colorbar()
+# plt.subplot(2, 1, 2)
+plt.figure()
 fig = plt.imshow(phase_map)
 plt.colorbar()
-plt.show()
-fig.set_cmap(‘spectral’)
-
+plt.show() 
+# fig.set_cmap("spectral")
 
